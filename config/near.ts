0aeee2523946890b54d6a25a1a0f13891ee465fa
TTNguyen <nttriet9a4@gmail.com>
2022-05-24 19:39:55 +0700

    update contract id

--- config/near.ts
+++ config/near.ts
@@ -7,7 +7,7 @@ export const NearConfig = Object.freeze({
         'https://rpc.testnet.internal.near.org',
     contractName:
         process.env.REACT_APP_NEAR_CONTRACT_NAME ||
-        'dev-1653278874223-16490255114160',
+        'dev-1653395903988-38288849707678',
     walletUrl:
         process.env.REACT_APP_NEAR_WALLET_URL ||
         'https://wallet.testnet.near.org',
